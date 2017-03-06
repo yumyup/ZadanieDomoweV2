c17b641f74e454f9a6756d0adc09d61ce0d63c9a
yupi <grafik@10g.pl>
2017-03-06 16:48:39 +0100

    dodanie klasy

---
+++ src/ZadanieDomoweV2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Helmut on 2017-03-06.
+ */
+public class ZadanieDomoweV2 {
+    public static void main (String args[]){
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programować");
+
+    }
+}
